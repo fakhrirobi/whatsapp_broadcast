1b0303246d00f6ea2c44585e560f31ec50d157fe
Fakhri Robi Aulia <fakhrirobi.fra@gmail.com>
2021-09-08 21:16:33 +0700

    adding credit page (linked to github)

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QHBoxLayout, QVBoxLayout, QWidget,QMainWindow,QBoxLayout
+from PyQt5.QtWebChannel import QWebChannel
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtCore import QUrl
 from datetime import datetime
 from PyQt5 import *
+from PyQt5.uic.uiparser import QtCore
 from functionality import *
 
 
@@ -19,6 +23,7 @@ class MainScreen(QDialog):
         self.BroadcastWithoutAtt.clicked.connect(self.broadcastWithoutAttachment)
         self.BroadcastWithAtt.clicked.connect(self.broadcastWithAttachment)
         self.FullProjectLink.clicked.connect(self.projectLink)
+        self.CloseButton.clicked.connect(self.exit_window)
         
     def usage_instruction(self):
         usage = Usage_Screen()
@@ -39,6 +44,8 @@ class MainScreen(QDialog):
         project_link = Full_Project_Link_Screen()
         widget.addWidget(project_link)
         widget.setCurrentIndex(widget.currentIndex()+1)
+    def exit_window(self,event) : 
+        sys.exit(app.exec_())
 class Usage_Screen(QDialog) : 
     def __init__(self):
         super(Usage_Screen, self).__init__()
@@ -69,56 +76,81 @@ class Broadcast_Without_Att_Screen(QDialog) :
         main_menu = MainScreen()
         widget.addWidget(main_menu)
         widget.setCurrentIndex(widget.currentIndex()+1)
+    
 class Broadcast_With_Att_Screen(QDialog) : 
     '''
     class for Defining Broadcast With Attachment Menu 
     '''
     def __init__(self):
-        #//TODO : CREATE CLASS EXPLANATION AS DOCSTRING
-        '''
         
-        '''
+ 
         super(Broadcast_With_Att_Screen, self).__init__()
-        loadUi("BroadcastWithDocs.ui",self)
-        self.ChooseRecipient.clicked.connect(self.clicked_choose_recipient)
-        self.ChooseAttachments.clicked.connect(self.choose_attachments)
-        self.SaveRecords.clicked.connect(self.save_records)
+        loadUi("ui_file/BroadcastWithDocs.ui",self)
         self.RunBroadcast.clicked.connect(self.run_broadcast)
         self.backButton.clicked.connect(self.backtomenu)
     def choose_attachments(self): 
-        #//TODO : CREATE choose_attachments EXPLANATION AS DOCSTRING
+        
         attachment_path = QFileDialog.getOpenFileName(self, 'Please Choose Attachment File ')
         return attachment_path
+    
     def clicked_choose_recipient(self) : 
-        #//TODO : CREATE clicked_choose_recipient EXPLANATION AS DOCSTRING
+        
         file_path = QFileDialog.getOpenFileName(self, 'Please Choose Recipient File (.xlsx file)')
         return file_path
     def save_records(self): 
-        #//TODO : CREATE save_records EXPLANATION AS DOCSTRING
+        
         message_content = self.MessageBox.toPlainText()
         return message_content
         
     def run_broadcast(self):
-        #//TODO : CREATE run_broadcast EXPLANATION AS DOCSTRING
+        '''
+        function to run broadcast flow : broadcast button clicked -> run_broadcast function -> broadcast functionality
+        '''
         recipient_path = self.clicked_choose_recipient()
         message_content = self.save_records()
         att_path =self.choose_attachments()
         broadcast_functionality().sent_messages_with_attachment(message_content,recipient_path,att_path)
         
     def backtomenu(self) : 
-        #//TODO : CREATE backtomenu EXPLANATION AS DOCSTRING
+        '''
+        function to back to main menu
+        '''
         main_menu = MainScreen()
         widget.addWidget(main_menu)
         widget.setCurrentIndex(widget.currentIndex()+1)
         
-        
+        #Photo by <a href="https://unsplash.com/@tricell1991?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Waranont (Joe)</a> on <a href="https://unsplash.com/s/photos/commercial-use?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
 class Full_Project_Link_Screen(QDialog) : 
     '''
     class for Showing Full Project in Github
     '''
     def __init__(self):
+        '''
+        https://stackoverflow.com/questions/66066115/render-markdown-with-pyqt5
+        '''
         super(Full_Project_Link_Screen, self).__init__()
-        loadUi("main_menu.ui",self)
+        loadUi('ui_file/ProjectLink.ui',self)
+        webview = QWebEngineView()
+        webview.setFixedHeight(500)
+        webview.setFixedWidth(500)
+        #// TODO : FIGURE OUT HOW TO SET GEOMETRY SIZE OF LAYOUT
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.setGeometry(0,0,800,900)
+        layout.addWidget(webview)
+        
+        webview.load(QUrl('https://github.com/fakhrirobi/whatsapp_broadcast#readme'))
+        self.backButton.clicked.connect(self.backtomenu)
+        
+    def backtomenu(self) : 
+        '''
+        function to back to main menu
+        '''
+        main_menu = MainScreen()
+        widget.addWidget(main_menu)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
